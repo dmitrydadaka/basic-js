aa86bb0e988cf1151c891bd553a33aab8f0ae3b3
User <belpc2012@gmail.com>
2021-10-01 16:36:37 +0300

    fix: two task solved

--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -16,7 +16,19 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
+export default function getMatrixElementsSum(/* m */) {
+  /* let arr=[]
+  for(let i=0;i<m.length;i++){
+    let a=[]
+    for(let j=0;j<m[1].length;j++){
+      a.push(m[j][i])
+    }
+    arr.push(a)
+    a=[]
+  }
+  return arr//.map((x,i)=>x.reverse()
+  //.slice(x.reverse().findIndex(y=>y==0)>-1?x.reverse().findIndex(y=>y==0):x.length)
+  //.reduce((a,c)=>a+c,0)).reduce((a,c)=>+c,0) */
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
 }
--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,7 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  let a=arr.filter(a=>a!=-1).sort((a,b)=>b-a)
+  return arr.map(x=>x==-1?-1:a.pop())
 }
